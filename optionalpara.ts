beec25da5b49eb0dee74da469eb9e93be1ea97fb
vhankadeyallawa <vhankaderence1992@gmail.com>
2023-03-07 17:39:08 +0530

    Basic concept learning optional parameter in Typescript
    Basic concept learning optional parameter in Typescript

---
+++ optionalpara.ts
@@ -0,0 +1,13 @@
+function display(id: number, name: string, role?: string) {
+    console.log('Id', id);
+    console.log('Name', name);
+    if(role != undefined) {
+        console.log('Role', role);// 
+    }
+
+
+}
+
+//display(1, 'Harry', 'Admin'); 
+
+display(2, 'Harmayni');
